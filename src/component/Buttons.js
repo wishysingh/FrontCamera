33594bf491b5f79fb013276903c39ac3464794b4
suyash <suyash9911103580@gmail.com>
2018-09-20 02:11:53 +0530

    bugfix

--- src/component/Buttons.js
+++ src/component/Buttons.js
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 const Buttons = ({click,value}) => {
     return(
         <div className='but'>
-            <Button variant="contained" color={`${value ? 'primary' : 'secondary'}`} className="but1" onClick={() => click(value)}>Click Me</Button>
+            <Button variant="contained" color={`${value ? 'secondary' : 'primary'}`} className="but1" onClick={() => click(value)}>Click Me</Button>
         </div>
     )
 }
